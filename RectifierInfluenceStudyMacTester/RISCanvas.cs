be3586054e47accc9dba88c22c26d15c0da46371
UrAHozerEh <kevjcronk@gmail.com>
2018-04-04 13:22:54 -0700

    Mergin some couple mac changes

---
+++ RectifierInfluenceStudyMacTester/RISCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RectifierInfluenceStudy;
+
+namespace RectifierInfluenceStudyMacTester
+{
+    public class RISCanvas : SkiaSharp.Views.Mac.SKCanvasView
+    {
+        public RISDataSet DataSet
+        {
+            get
+            {
+                return DataSet;
+            }
+            set
+            {
+                DataSet = value;
+                this.NeedsDisplay = true;
+            }
+        }
+
+
+    }
+}
