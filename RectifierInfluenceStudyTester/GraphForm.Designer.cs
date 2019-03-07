e78fb37bac9caf2e053c408151e2cd51a23d1882
KevinC <kcronk7523@hotmail.com>
2019-03-07 16:42:27 -0700

    Adding Excel interop

--- RectifierInfluenceStudyTester/GraphForm.Designer.cs
+++ RectifierInfluenceStudyTester/GraphForm.Designer.cs
@@ -34,8 +34,12 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.ListFiles = new System.Windows.Forms.ListBox();
             this.TextOffset = new System.Windows.Forms.TextBox();
+            this.CheckApproved = new System.Windows.Forms.CheckBox();
+            this.ListFiles = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ButtonExportOne = new System.Windows.Forms.Button();
+            this.ButtonExportApproved = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -52,7 +56,7 @@
             this.groupBox1.Controls.Add(this.splitContainer1);
             this.groupBox1.Location = new System.Drawing.Point(1, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(737, 433);
+            this.groupBox1.Size = new System.Drawing.Size(737, 323);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
@@ -69,7 +73,7 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.ListFiles);
-            this.splitContainer1.Size = new System.Drawing.Size(731, 414);
+            this.splitContainer1.Size = new System.Drawing.Size(731, 304);
             this.splitContainer1.SplitterDistance = 591;
             this.splitContainer1.TabIndex = 0;
             // 
@@ -88,40 +92,87 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.Chart.Series.Add(series1);
-            this.Chart.Size = new System.Drawing.Size(585, 408);
+            this.Chart.Size = new System.Drawing.Size(585, 304);
             this.Chart.TabIndex = 0;
             this.Chart.Text = "chart1";
+            // 
+            // TextOffset
+            // 
+            this.TextOffset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.TextOffset.Location = new System.Drawing.Point(7, 370);
+            this.TextOffset.Name = "TextOffset";
+            this.TextOffset.Size = new System.Drawing.Size(100, 20);
+            this.TextOffset.TabIndex = 1;
+            this.TextOffset.TextChanged += new System.EventHandler(this.TextOffset_TextChanged);
+            // 
+            // CheckApproved
+            // 
+            this.CheckApproved.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CheckApproved.AutoSize = true;
+            this.CheckApproved.Location = new System.Drawing.Point(7, 329);
+            this.CheckApproved.Name = "CheckApproved";
+            this.CheckApproved.Size = new System.Drawing.Size(72, 17);
+            this.CheckApproved.TabIndex = 2;
+            this.CheckApproved.Text = "Approved";
+            this.CheckApproved.UseVisualStyleBackColor = true;
+            this.CheckApproved.CheckedChanged += new System.EventHandler(this.CheckApproved_CheckedChanged);
             // 
             // ListFiles
             // 
             this.ListFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ListFiles.FormattingEnabled = true;
-            this.ListFiles.Location = new System.Drawing.Point(4, 4);
+            this.ListFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.ListFiles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.ListFiles.Location = new System.Drawing.Point(3, 0);
+            this.ListFiles.MultiSelect = false;
             this.ListFiles.Name = "ListFiles";
-            this.ListFiles.Size = new System.Drawing.Size(132, 407);
-            this.ListFiles.TabIndex = 0;
-            this.ListFiles.SelectedIndexChanged += new System.EventHandler(this.ListFiles_SelectedIndexChanged);
+            this.ListFiles.Size = new System.Drawing.Size(133, 301);
+            this.ListFiles.TabIndex = 3;
+            this.ListFiles.UseCompatibleStateImageBehavior = false;
+            this.ListFiles.View = System.Windows.Forms.View.Details;
+            this.ListFiles.SelectedIndexChanged += new System.EventHandler(this.ListFiles_SelectedIndexChanged_1);
             // 
-            // TextOffset
+            // columnHeader2
             // 
-            this.TextOffset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.TextOffset.Location = new System.Drawing.Point(7, 480);
-            this.TextOffset.Name = "TextOffset";
-            this.TextOffset.Size = new System.Drawing.Size(100, 20);
-            this.TextOffset.TabIndex = 1;
-            this.TextOffset.TextChanged += new System.EventHandler(this.TextOffset_TextChanged);
+            this.columnHeader2.Width = 999;
+            // 
+            // ButtonExportOne
+            // 
+            this.ButtonExportOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonExportOne.Location = new System.Drawing.Point(624, 367);
+            this.ButtonExportOne.Name = "ButtonExportOne";
+            this.ButtonExportOne.Size = new System.Drawing.Size(102, 23);
+            this.ButtonExportOne.TabIndex = 3;
+            this.ButtonExportOne.Text = "Export";
+            this.ButtonExportOne.UseVisualStyleBackColor = true;
+            this.ButtonExportOne.Click += new System.EventHandler(this.ButtonExportOne_Click);
+            // 
+            // ButtonExportApproved
+            // 
+            this.ButtonExportApproved.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonExportApproved.Location = new System.Drawing.Point(624, 338);
+            this.ButtonExportApproved.Name = "ButtonExportApproved";
+            this.ButtonExportApproved.Size = new System.Drawing.Size(102, 23);
+            this.ButtonExportApproved.TabIndex = 4;
+            this.ButtonExportApproved.Text = "Export Approved";
+            this.ButtonExportApproved.UseVisualStyleBackColor = true;
             // 
             // GraphForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 512);
+            this.ClientSize = new System.Drawing.Size(738, 402);
+            this.Controls.Add(this.ButtonExportApproved);
+            this.Controls.Add(this.ButtonExportOne);
+            this.Controls.Add(this.CheckApproved);
             this.Controls.Add(this.TextOffset);
             this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
             this.Name = "GraphForm";
             this.Text = "GaphForm";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.GraphForm_KeyPress);
             this.groupBox1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -138,7 +189,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
-        private System.Windows.Forms.ListBox ListFiles;
         private System.Windows.Forms.TextBox TextOffset;
+        private System.Windows.Forms.CheckBox CheckApproved;
+        private System.Windows.Forms.ListView ListFiles;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button ButtonExportOne;
+        private System.Windows.Forms.Button ButtonExportApproved;
     }
 }
--- RectifierInfluenceStudyTester/GraphForm.cs
+++ RectifierInfluenceStudyTester/GraphForm.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using RectifierInfluenceStudy;
+using Microsoft.Office.Interop.Excel;
+using ChartAreaForm = System.Windows.Forms.DataVisualization.Charting.ChartArea;
+using SeriesForm = System.Windows.Forms.DataVisualization.Charting.Series;
+using FontForm = System.Drawing.Font;
+using ExcelApplication = Microsoft.Office.Interop.Excel.Application;
+using ExcelChart = Microsoft.Office.Interop.Excel.Chart;
 
 namespace RectifierInfluenceStudyTester
 {
@@ -25,11 +32,17 @@ namespace RectifierInfluenceStudyTester
         private double _SpanMin = double.MaxValue;
         private double _SpanMax = double.MinValue;
         private bool _AutoScale = true;
+        private int SelectedIndex => ListFiles.SelectedIndices.Count == 0 ? -1 : ListFiles.SelectedIndices[0];
+        private string SelectedFullName => _Sets[SelectedIndex].FullFileName;
+        private int _OldIndex = -1;
+        private ExcelApplication _Excel = new ExcelApplication();
 
         public GraphForm()
         {
             InitializeComponent();
             Chart.Series.Clear();
+            _Excel.Visible = false;
+            _Excel.DisplayAlerts = false;
         }
 
         public void UpdateSets()
@@ -74,7 +87,7 @@ namespace RectifierInfluenceStudyTester
                 _SpanMax = 0 - _SpanMin;
             else
                 _SpanMin = 0 - _SpanMax;
-            ChartArea area = Chart.ChartAreas[0];
+            ChartAreaForm area = Chart.ChartAreas[0];
             area.AxisX.Minimum = 0;
             area.AxisX.Maximum = Cycle.Length.TotalSeconds;
             if (!_AutoScale)
@@ -83,16 +96,16 @@ namespace RectifierInfluenceStudyTester
                 area.AxisY.Minimum = _Minimum;
             }
             area.AxisY.IsReversed = true;
-            ListFiles.SelectedIndex = 0;
+            //ListFiles.SelectedIndices = 0;
         }
 
         private void ChangeToSet(int pIndex)
         {
-            List<Series> series = CreateSeries(_Sets[pIndex]);
+            List<SeriesForm> series = CreateSeries(_Sets[pIndex]);
             Chart.Series.Clear();
             //TextOffset.Text = _Offset[_Sets[pIndex].FullFileName].ToString("F1");
             if (series == null) return;
-            ChartArea area = Chart.ChartAreas[0];
+            ChartAreaForm area = Chart.ChartAreas[0];
             if (!_AutoScale)
             {
                 if (_Sets[pIndex].FileName.ToLower().Contains("span"))
@@ -106,21 +119,27 @@ namespace RectifierInfluenceStudyTester
                     area.AxisY.Minimum = _Minimum;
                 }
             }
-            foreach (Series s in series)
+            else
+            {
+                area.RecalculateAxesScale();
+                area.AxisX.Minimum = 0;
+                area.AxisX.Maximum = Cycle.Length.TotalSeconds;
+            }
+            foreach (SeriesForm s in series)
             {
                 Chart.Series.Add(s);
             }
         }
 
-        private List<Series> CreateSeries(RISDataSet pSet)
+        private List<SeriesForm> CreateSeries(RISDataSet pSet)
         {
-            List<Series> collection = new List<Series>();
+            List<SeriesForm> collection = new List<SeriesForm>();
             pSet.Offset = _Offset[pSet.FullFileName];
             pSet.CreateGraphReads();
             MultiSetInterruptionCycle cycle = Cycle as MultiSetInterruptionCycle;
             foreach (RectifierSet set in cycle.Sets)
             {
-                Series s = new Series(set.Name);
+                SeriesForm s = new SeriesForm(set.Name);
                 s.ChartType = SeriesChartType.Line;
                 if (set.Name != "All On")
                     s.BorderWidth = 3;
@@ -155,29 +174,115 @@ namespace RectifierInfluenceStudyTester
                     collection[0].Points.AddXY(read2.Time, read2.Value);
                 }
             }
-            foreach (Series s in collection)
+            foreach (SeriesForm s in collection)
                 s.Sort(PointSortOrder.Ascending, "X");
             return collection;
         }
 
         private void ListFiles_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int index = ListFiles.SelectedIndex;
-            ChangeToSet(index);
-            TextOffset.Text = _Offset[_Sets[index].FullFileName].ToString("F1");
+            if (SelectedIndex == -1)
+                return;
+            ChangeToSet(SelectedIndex);
+            TextOffset.Text = _Offset[SelectedFullName].ToString("F1");
         }
 
         private void TextOffset_TextChanged(object sender, EventArgs e)
         {
             double offset;
             string text = TextOffset.Text;
-            int index = ListFiles.SelectedIndex;
             if (string.IsNullOrWhiteSpace(text) || !double.TryParse(text, out offset))
                 return;
-            if (_Offset[_Sets[index].FullFileName] == offset)
+            if (_Offset[SelectedFullName] == offset)
                 return;
-            _Offset[_Sets[index].FullFileName] = offset;
-            ChangeToSet(index);
+            _Offset[SelectedFullName] = offset;
+            ChangeToSet(SelectedIndex);
+        }
+
+        private void CheckApproved_CheckedChanged(object sender, EventArgs e)
+        {
+            bool approved = CheckApproved.Checked;
+            if (approved == _Approved[SelectedFullName])
+                return;
+            _Approved[SelectedFullName] = approved;
+            ListFiles.Items[SelectedIndex].BackColor = approved ? Color.LightGreen : Color.White;
+            ListFiles.Items[SelectedIndex].Selected = true;
+        }
+
+        private void ListFiles_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (SelectedIndex == -1)
+                return;
+            ChangeToSet(SelectedIndex);
+            TextOffset.Text = _Offset[SelectedFullName].ToString("F1");
+            FontForm font = new FontForm(ListFiles.Items[SelectedIndex].Font, FontStyle.Bold);
+            if (_OldIndex != -1)
+                ListFiles.Items[_OldIndex].Font = ListFiles.Items[SelectedIndex].Font;
+            ListFiles.Items[SelectedIndex].Font = font;
+            CheckApproved.Checked = _Approved[SelectedFullName];
+            _OldIndex = SelectedIndex;
+        }
+
+        private void GraphForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+                CheckApproved.Checked = true;
+        }
+
+        private void ButtonExportOne_Click(object sender, EventArgs e)
+        {
+            RISDataSet set = _Sets[SelectedIndex];
+            ExcelExport(set);
+        }
+
+        private void ExcelExport(RISDataSet pSet)
+        {
+            string fileName = Path.ChangeExtension(pSet.FullFileName, ".xlsx");
+            Workbook workbook = _Excel.Workbooks.Add(Type.Missing);
+            Worksheet worksheet = workbook.ActiveSheet as Worksheet;
+
+            worksheet.Name = "Data";
+
+            List<SeriesForm> series = CreateSeries(pSet);
+            int cycleNum = 1;
+            double lastX = double.MinValue;
+            int row = 2;
+            int pointIndex = 0;
+
+            foreach (DataPoint point in series[0].Points)
+            {
+                if (cycleNum < series.Count && series[cycleNum].Points.Count == pointIndex)
+                {
+                    ++cycleNum;
+                    pointIndex = 0;
+                }
+                if (cycleNum < series.Count && point.XValue == series[cycleNum].Points[pointIndex].XValue)
+                {
+                    worksheet.Cells[row, cycleNum + 2] = series[cycleNum].Points[pointIndex].YValues[0];
+                    ++pointIndex;
+                }
+                if (point.XValue < lastX)
+                    continue;
+                worksheet.Cells[row, 1] = point.XValue;
+                worksheet.Cells[row, 2] = point.YValues[0];
+                ++row;
+                lastX = point.XValue;
+            }
+            Range range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[row - 1, series.Count + 1]];
+            ExcelChart chart = workbook.Charts.Add(Type.Missing) as ExcelChart;
+            chart.Name = "Chart";
+            chart.ChartType = XlChartType.xlLine;
+            chart.ChartStyle = 227;
+            chart.SetSourceData(range);
+            chart.HasTitle = true;
+            chart.ChartTitle.Text = pSet.FileName;
+
+
+
+            workbook.SaveAs(fileName);
+            workbook.Close();
+            worksheet = null;
+            workbook = null;
         }
     }
 }
